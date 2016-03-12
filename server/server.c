4e51a780c10678999b3a3e2873ff385f9683bda5
emma.moriceau <emma.moriceau@epita.fr>
2016-03-12 19:41:24 +0100

    server pb pollin

--- server/server.c
+++ server/server.c
@@ -1,11 +1,9 @@
 #include <errno.h>
 #include "server.h"
 
-//port user?
-//define backlog nb maxi de connexions acceptees
-
 void server() {
   int sock_fd; //socket ecoutee
+  int io= 1;
   int new_fd, reuse = 1, n;  //nouvelle connexion
   char buffer[256];
   struct addrinfo hints, *resinfo= NULL;
@@ -29,19 +27,18 @@ void server() {
   if (getaddrinfo(NULL, "4243", &hints, &resinfo) != 0)
     err(1, "err getaddrinfo");
 
-  if((sock_fd= socket(resinfo->ai_family, resinfo->ai_socktype, resinfo->ai_protocol)) > 0)
-    printf("the socket is created\n");
-  fds[0].fd= sock_fd;
+  if((sock_fd= socket(resinfo->ai_family, resinfo->ai_socktype, resinfo->ai_protocol)) < 0)
+    err(1, "err socket");
+ 
   if(setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1)
     err(1, "setsockopt problem");
 
-  if(bind(sock_fd, resinfo->ai_addr, resinfo->ai_addrlen) == 0)
-    printf("binding socket\n");
+  if(bind(sock_fd, resinfo->ai_addr, resinfo->ai_addrlen) != 0)
+    err(1, "err bind");
 
   freeaddrinfo(resinfo);
+  
   if(!listen(sock_fd, 100)) {
-    printf("listening\n");
-    //loop for poll
     if(poll(fds, reuse, 1000) < 0)
       perror("poll error");
   }
@@ -49,45 +46,34 @@ void server() {
     printf("%d\n", sock_fd);
 
   while(1) {
+    fds[0].fd= sock_fd;
     poll(fds, reuse, 1000000);
-    for(int i = 0; i < reuse; i++)
-      {
-        if (fds[i].revents != POLLIN)
-          printf("ca marche pas pollin\n");
-        ioctl(fds[0].fd, FIONBIO, 1);
-        printf("%d\n",i);
-        if (fds[i].fd == fds[0].fd)
-          {
-            do
-              {
-                if (reuse == 4)
-                  break;
-                printf("coucou ca va la famille? reuse vaut : %d\n", reuse);
-                new_fd = accept(fds[i].fd, (struct sockaddr*)NULL, NULL);
-                if (new_fd == -1)
-                  break;
-                fds[reuse].fd = new_fd;
-                fds[reuse].events = POLLIN;
-                reuse++;
-              } while (new_fd != -1);
-          }
-        else
-          {
-            /* ioctl(fds[i].fd, FIONREAD, &n); */
-            /* if (n < 1) */
-            /*   { */
-            /*     close(fds[i].fd); */
-            /*     fds[i].fd = -1; */
-            /*   } */
-            /* else */
-            /*   { */
-                if ((n = read(fds[i].fd, buffer, 256)) < 0)
-                  close(fds[i].fd);
-                printf("%s\n", buffer);
-                memset(buffer, 0, 256);
-                /* } */
-          }
+    
+    for(int i = 0; i < reuse; i++) {
+      if (fds[i].revents != POLLIN)
+      	perror("pollin error");
+      
+      ioctl(fds[0].fd, FIONBIO, &io);
+      
+      if (fds[i].fd == fds[0].fd) {
+      	do {     
+          new_fd = accept(fds[0].fd, (struct sockaddr*)NULL, NULL);
+          if (new_fd == -1)
+             break;
+          fds[reuse].fd = new_fd;
+          fds[reuse].events = POLLIN;
+          reuse++;
+        } while (new_fd != -1);
       }
+      else {
+	while(1) {
+	  if((n = read(fds[i].fd, buffer, 256)) < 0)   
+	    close(fds[i].fd);
+	  printf("%s\n", buffer);
+	  memset(buffer, 0, 256);
+	}		
+      }
+    }
   }
 }
 
