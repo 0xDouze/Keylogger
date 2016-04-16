e0d7738dd951819dd955a0a004f7e808b3884f07
Celine LY <celine1.ly@epita.fr>
2016-04-16 12:02:03 +0200

    Interface : Ajout d'une barre de defilement pour la liste des clients

--- server/interface/mainWindow.c
+++ server/interface/mainWindow.c
@@ -20,10 +20,14 @@ static GtkTreeModel *create_fill_list_clients(void)
 
 	gtk_list_store_append (store, &iter);
 	gtk_list_store_set (store, &iter, COL_CLIENT, "Emma", -1);
-
-	gtk_list_store_append (store, &iter);
-	gtk_list_store_set (store, &iter, COL_CLIENT, "Kevin", -1);
 	
+	// A supprimer plus tard 
+	int i = 0;
+	for (i = 0; i < 100; i++)
+	{
+		gtk_list_store_append (store, &iter);
+		gtk_list_store_set (store, &iter, COL_CLIENT, "Kevin", -1);
+	}
 
 	return GTK_TREE_MODEL(store); 
 }
@@ -53,17 +57,20 @@ int main (int argc, char **argv)
 {
 	GtkWidget *mainWindow;
 	GtkWidget *listClients;
+	GtkWidget *scrollbar;
 	
 	gtk_init(&argc, &argv);
 
 	mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 1000, 700);
+	gtk_window_set_default_size(GTK_WINDOW(mainWindow), 1000, 500);
 	gtk_window_set_position(GTK_WINDOW(mainWindow), GTK_WIN_POS_CENTER);
 	g_signal_connect(G_OBJECT(mainWindow), "delete_event", G_CALLBACK(gtk_main_quit), NULL);
 	
 	listClients = create_view_model();
-
-	gtk_container_add(GTK_CONTAINER(mainWindow), listClients);
+	scrollbar = gtk_scrolled_window_new(NULL, NULL);
+	
+	gtk_container_add(GTK_CONTAINER(scrollbar), listClients);
+	gtk_container_add(GTK_CONTAINER(mainWindow), scrollbar);
 	
 	gtk_widget_show_all(mainWindow);
 
